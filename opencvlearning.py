282fdab0b7e1c4200b58cf4b9030b8aea6a14236
JjIiNnXx <1823299082@qq.com>
2022-11-14 14:36:24 +0800

    change

--- opencvlearning.py
+++ opencvlearning.py
@@ -1,8 +1,8 @@
 import  cv2 as cv
 
 print("hello")
-# img=cv.imread("Photos/twocats.jpeg")
-# img=cv.resize(img,(500,400))
-# cv.imshow("img",img)
-# cv.waitKey(0)
-# cv.destroyAllWindows()
+img=cv.imread("Photos/twocats.jpeg")
+img=cv.resize(img,(500,400))
+cv.imshow("img",img)
+cv.waitKey(0)
+cv.destroyAllWindows()
